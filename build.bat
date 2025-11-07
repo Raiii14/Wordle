a3dca968fdc2b98cb756d688b5b46edc984d0c66
clark <clarkjmv.2006@gmail.com>
2025-11-07 10:55:44 +0800

    updated build script

--- build.bat
+++ build.bat
@@ -1,20 +1,13 @@
 @ECHO OFF
-TASM mainModu\wordle.asm || GOTO :error
-TASM services\drawbox.asm || GOTO :error
-TASM services\getchar.asm, getchar.obj || GOTO :error
-TASM services\validate.asm || GOTO :error
-TASM services\logic.asm || GOTO :error
-TASM services\fillbox.asm || GOTO :error
-TASM services\getword.asm || GOTO :error
+TASM mainModu\wordle.asm
+TASM services\drawbox.asm
+TASM services\getchar.asm, getchar.obj
+TASM services\validate.asm
+TASM services\logic.asm
+TASM services\fillbox.asm
+TASM services\getword.asm
 
-TLINK wordle.obj+drawbox.obj+getchar.obj+validate.obj+logic.obj+fillbox.obj+getword.obj || GOTO :error
-wordle || GOTO :error
+TLINK wordle.obj+drawbox.obj+getchar.obj+validate.obj+logic.obj+fillbox.obj+getword.obj
+wordle
 
-GOTO :end
-
-:error
-ECHO Error: Premature exit.
-GOTO :end
-
-:end
 ECHO Done.
